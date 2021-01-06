6f2c28b6712dd80a1981d3d089e37c229eac6cdf
jpierce424 <71786124+jpierce424@users.noreply.github.com>
2021-01-06 11:13:39 -0500

    Add files via upload

---
+++ output lab java.js
@@ -0,0 +1,40 @@
+// Enter your code below to edit index.html
+
+yourName = "Jathan Pierce"
+document.getElementById("yourName").innerHTML = yourName;
+
+title="My bio"
+document.getElementById("title").innerHTML = title;
+
+myBio="I like to play video games and thats all I do every day and I also like the Pittsburg Steelers"
+document.getElementById("myBio").innerHTML = myBio;
+
+collegeChoice="Ohio State"
+document.getElementById("collegeChoice").innerHTML = collegeChoice;
+
+programChoice="Software Engineering"
+document.getElementById("programChoice").innerHTML = programChoice;
+
+collegeReason="I would like to go here because it is a very good school and it would help a lot in my career."
+document.getElementById("collegeReason").innerHTML = collegeReason;
+
+careerChoice="Web designing"
+document.getElementById("careerChoice").innerHTML = careerChoice;
+
+companyChoice="I would like to work at Amazon or Google"
+document.getElementById("companyChoice").innerHTML = companyChoice;
+
+careerReason="They pay really well and its something I like"
+document.getElementById("careerReason").innerHTML = careerReason;
+
+careerReason="They pay really well and its something I like"
+document.getElementById("careerReason").innerHTML = careerReason;
+
+answerOne="They pay It is a cross-playform iterface that treats or XML as a tree structure to represent a document  "
+document.getElementById("answerOne").innerHTML = answerOne;
+
+answerTwo="Writting into Html Element, writing into an alert box, the browser console"
+document.getElementById("answerTwo").innerHTML = answerTwo;
+
+answerThree="You can change or manipulate all Html elements, and Css"
+document.getElementById("answerThree").innerHTML = answerThree;
